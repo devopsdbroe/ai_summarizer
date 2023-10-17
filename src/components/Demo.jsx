b5f739015ed628d7afb31907c145e1906b861524
devopsdbroe <dbroe1995@gmail.com>
2023-10-17 14:23:10 -0400

    Implemented API and local storage

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
+
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
 	const [article, setArticle] = useState({
@@ -7,8 +9,34 @@ const Demo = () => {
 		summary: "",
 	});
 
+	const [allArticles, setAllArticles] = useState([]);
+
+	const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+	useEffect(() => {
+		const articlesFromLocalStorage = JSON.parse(
+			localStorage.getItem("articles")
+		);
+
+		if (articlesFromLocalStorage) {
+			setAllArticles(articlesFromLocalStorage);
+		}
+	}, []);
+
 	const handleSubmit = async (e) => {
-		alert("Submitted");
+		e.preventDefault();
+
+		const { data } = await getSummary({ articleUrl: article.url });
+
+		if (data?.summary) {
+			const newArticle = { ...article, summary: data.summary };
+			const updatedAllArticles = [newArticle, ...allArticles];
+
+			setAllArticles(updatedAllArticles);
+			setArticle(newArticle);
+
+			localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+		}
 	};
 
 	return (
